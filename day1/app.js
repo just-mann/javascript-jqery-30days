c46ba0da5ca2d35d185662981ae1957b878a0228
Justin <64009695+just-mann@users.noreply.github.com>
2022-06-25 19:55:48 -0400

    changes made

--- day1/app.js
+++ day1/app.js
@@ -1,1 +1,10 @@
 console.log("It works...");
+
+let width, height, area;
+
+width = 4;
+height = 5;
+
+area = `The Area is: ` + width * height + `.`;
+
+document.getElementById('demo').innerHTML = area;
