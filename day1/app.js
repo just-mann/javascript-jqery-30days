71095600c12c1d4e40c5c0feb5521e17ec956591
Justin <64009695+just-mann@users.noreply.github.com>
2022-06-27 09:45:30 -0400

    day1 ended
    Day 1 ended
    Unfortunately didn't do Day2
    But back and ready on Day3

--- day1/app.js
+++ day1/app.js
@@ -1,10 +1,54 @@
 console.log("It works...");
+let btn = document.getElementById('clickBtn');
 
-let width, height, area;
+let width, height, area, local;
 
 width = 4;
 height = 5;
+local = 5;
 
-area = `The Area is: ` + width * height + `.`;
+area = (2 + 4) * 10; // 60
 
 document.getElementById('demo').innerHTML = area;
+
+
+
+
+// Arrays
+let cars = new Array(
+     'BMW',
+     'Tesla',
+     'Honda',
+     'Toyota',
+     'Range Rover',
+     'Acura',
+     'Audi',
+     'Ford'
+);
+
+function displCars() {
+     let randomCar = document.querySelector('#displayCars');
+     randomCar.textContent = cars[Math.floor((Math.random() * cars.length))].toUpperCase();
+}
+
+btn.addEventListener('click', displCars);
+
+
+
+// Function and Methods ( Page: #90 )
+function getArea(width, height) {
+    return width * height; 
+}
+
+// getArea(3, 5);
+
+let wallOne = getArea(3, 7); // 21
+let el = document.getElementById('message1');
+el.textContent = wallOne;
+let wallTwo = getArea(6, 5); // 30
+let el2 = document.getElementById('message2');
+el2.textContent = wallTwo;
+
+
+
+
