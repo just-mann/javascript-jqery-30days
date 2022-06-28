65a35f2864a3ce9ab144048512e73253e81b34ea
Justin <64009695+just-mann@users.noreply.github.com>
2022-06-28 10:05:46 -0400

    Update app.js

--- day3/app.js
+++ day3/app.js
@@ -3,19 +3,18 @@ console.log('It works...');
 let button = document.querySelector('button');
 
 var hotel = {
-     name: 'Quay',
-     rooms: 40,
-     booked: 25,
-     gym: true,
+     name: 'Quay', // The name of this hotel
+     rooms: 40, // Total Number of Rooms in the 'Quay' hotel
+     booked: 25, // Rooms booked out of the total number of rooms ()
+     gym: true, // This hotel has a gym
      roomsType: ['twin', 'double', 'suite'],
-     checkAvailability: function() {
-          return this.room - this.booked;
+     checkAvailability: function() { // Calculating the number of rooms available
+          return this.room - this.booked; 
           // return total;
      }
 };
+
 console.log(hotel.checkAvailability);
-
-
 
 // button.addEventListener('click', checkAvai);
 
